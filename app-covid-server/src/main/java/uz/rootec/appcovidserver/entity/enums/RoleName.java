48b1ab3137246b10590f464b33071e18037be1ee
1Sherlock <=>
2021-09-04 22:46:06 +0500

    'd'

---
+++ app-covid-server/src/main/java/uz/rootec/appcovidserver/entity/enums/RoleName.java
@@ -0,0 +1,6 @@
+package uz.rootec.appcovidserver.entity.enums;
+
+public enum RoleName {
+    ROLE_DIRECTOR,
+    ROLE_WORKER,
+}
---
+++ app-covid-server/src/test/java/uz/rootec/appcovidserver/AppCovidServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.rootec.appcovidserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCovidServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
